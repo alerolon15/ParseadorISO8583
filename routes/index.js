39901515c1178a26652c9dbc52a94dbfdda3e494
Alejandro Rolón Parodi <ARolonParodi@coasa.com.ar>
2017-12-01 17:02:48 -0300

    arreglo de comentarios

--- routes/index.js
+++ routes/index.js
@@ -43,42 +43,38 @@ router.post('/Enviar', function(req, res, next) {
     console.log(ip, puerto)
 
     client.connect(puerto, ip, function() {
-      //para TCPP hay que enviar el tamaño del mensaje en la cabecera del mensaje
-      var length = iso.length;
-
-      // en caso de hacer una conexion TCPP descomentar la linea de abajo y comentar la de TCP
 
       if (tcpp) {
+        // en caso de hacer una conexion TCPP se envia el tamaño del mensaje como cabecera
+        var length = iso.length;
         client.write(length + iso);
       }else{
         // en caso de hacer una conexion TCP descomentar la linea de abajo y comentar la de TCPP
         client.write(iso);
       }
-
     });
 
+    // En caso de recibir informacion por el socket
     client.on('data', function(data) {
-
       console.log('Received: ' + data);
-
       var datos = data.toString('ascii');
-
       //client.destroy(); // kill client after server's response
       if (datos.substr(0,3) == "ISO") {
         res.send({ ISO: datos });
       }
     });
 
+    // En caso de recibir error
     client.on('error', function(data) {
       console.log(data);
-
       if (data.code) {
         res.send({ error: '<div class="card-panel red darken-2" onclick="Cerrar()" style="color: rgba(255, 255, 255, 0.9);"><span>Algo Salió Mal: no se pudo conectar a la ip y puerto establecida. Error: ' + data.code + '</span><i class="material-icons right">close</i></div>' })
       }
     });
+
+    // En caso de recibir evento de cierre de socket
     client.on('close', function() {
       console.log('Conexion Cerrada!');
-      //res.send({ error: '<div class="card-panel red darken-2" style="color: rgba(255, 255, 255, 0.9);"><span>Algo Salió Mal: no se pudo establecer una conexion al ip y puerto indicado.</span></div>' });
     });
   }
 });
